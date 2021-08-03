72784bee888839200390749f94a2cb444c9e78ad
Ayush Sur <Ayush.Sur@ibm.com>
2021-08-03 14:50:42 +0530

    Added Table component, basic Vaccination dashboard

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import { Router, Route, Switch } from "react-router-dom";
 import history from './history';
 import AddUpdateHospital from '../components/hospitals/addUpdateHospital';
+import VaccinationDashboard from "../components/vaccinationDashboard";
 
 function Routes() {
     return (
@@ -8,6 +9,7 @@ function Routes() {
             <Router history={history}>
                 <Switch>
                   <Route component={AddUpdateHospital} path="/addupdatehospital" />
+                  <Route component={VaccinationDashboard} path="/vaccination"/>
                 </Switch>
             </Router>
         </div>
