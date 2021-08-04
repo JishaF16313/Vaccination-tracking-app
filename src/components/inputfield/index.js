fe4353fef03fb4ef54bbbc4df0e873daa4d68c85
Sunny Tiwari <Sunny.Tiwari1@ibm.com>
2021-08-04 15:28:40 +0530

    Added screen for adding hospital user in the application

---
+++ src/components/inputfield/index.js
@@ -0,0 +1,28 @@
+import { useField, ErrorMessage } from 'formik';
+import { TextField, MenuItem } from "@material-ui/core";
+
+function InputField ({ label, classes, ...props }) {
+    const [field, meta] = useField(props);
+    return (
+        <div>
+            {(props.type === 'text' || props.type === 'password') && (
+                <TextField label={label} onChange={props.onChange} className={`form-control ${meta.touched && meta.error && 'is-valid'}`} {...field} {...props} autoComplete="off" />
+            )}
+            {props.type === 'textarea' && (
+                <TextField label={label} multiline maxRows={3} onChange={props.onChange} className={`form-control ${classes.addressField} ${meta.touched && meta.error && 'is-valid'}`} {...field} {...props} autoComplete="off" />
+            )}
+            {props.type === 'select' && (
+                <TextField label={label} select className={classes.ddl} onChange={props.onChange}>
+                    {props.options.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                        </MenuItem>
+                    ))}
+                </TextField>
+            )}
+            <ErrorMessage component="div" name={field.name} className={classes.errorField} />
+        </div>
+    )
+}
+
+export default InputField;
