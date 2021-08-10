5892c3c0e99300e615f1abab433264752b38f47e
Ayush Sur <Ayush.Sur@ibm.com>
2021-08-10 11:30:31 +0530

    preparing sigin integration

--- src/store/actions/auth/index.js
+++ src/store/actions/auth/index.js
@@ -1,4 +1,6 @@
 import users from "../../../lib/mocks/users.json"
+import * as API from "../../../lib/api"
+import {parseJwt} from "../../../utility/commonFunctions"
 
 // Authentication
 export const AUTH_INIT = "AUTH_INIT"
@@ -15,10 +17,17 @@ export const checkAuth = () => dispatch => {
 
 export const authenticate = (credentials) => dispatch => {
     dispatch(authInit())
-    setTimeout(() =>{
-        const user = users.find(user => user.email === credentials.userName)
+    if(users.findIndex(user => user.email === credentials.userLoginId) >= 0)
+    {
+    return setTimeout(() =>{
+        const user = users.find(user => user.email === credentials.userLoginId)
         user ? dispatch(authSuccess(user)) : dispatch(authFail("Invalid credentials"))
     }, 500)
+    }
+    API.API_PUT_SERVICE("/auth/login",credentials).then( token => {
+        const user = parseJwt(token)
+        dispatch(authSuccess({...user, jwt: token}))
+    })
 }
 const authInit = () => ({
     type: AUTH_INIT
