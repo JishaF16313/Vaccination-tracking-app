9c0c00efd4df4d1d824dd4592b0d9a080ad65e12
Anushka Roy <anushka.roy@ibm.com>
2021-08-26 19:13:09 +0530

    Exception Handling for Pending status booking & added latest API endpoints for BBS, BAS, Vaccination & User service

--- src/env-config.js
+++ src/env-config.js
@@ -1,8 +1,8 @@
 export const USER_REGISTER_V1 = '_signup';
 
 export const USER_SERVICE = process.env.REACT_APP_USER_SERVICE || 'http://covid-user-svc2-sssonikasingh-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/user/';
-export const BED_AVAILABILITY_SERVICE = process.env.REACT_APP_BED_AVAILABILITY_SERVICE || 'https://bas-svc-route-kamlesh-k-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/bas/';
-export const VACCINATION_SERVICE = process.env.REACT_APP_VACCINATION_SERVICE || 'http://9.43.86.239:8090/vaccine/';
-export const BEDBOOKING_SERVICE = process.env.REACT_APP_BEDBOOKING_SERVICE || 'https://bbs-svc-route-kamlesh-k-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/bbs/'
+export const BED_AVAILABILITY_SERVICE = process.env.REACT_APP_BED_AVAILABILITY_SERVICE || 'http://bas-svc-bharathkumar-e4-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/bas/';
+export const VACCINATION_SERVICE = process.env.REACT_APP_VACCINATION_SERVICE || 'http://bas-svc-bharathkumar-e4-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/vaccine/';
+export const BEDBOOKING_SERVICE = process.env.REACT_APP_BEDBOOKING_SERVICE || 'http://covid-bbs-svc8-sssonikasingh-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/bbs/'
 export const REPORT_SERVICE = process.env.REACT_APP_REPORT_SERVICE || 'https://report-svc-route-kamlesh-k-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/report/';
 
